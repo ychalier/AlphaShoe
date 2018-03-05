448041d25e126c14de0061d0a7caf5e0c02dd1e7
ychalier <yohan.chalier@telecom-paristech.fr>
2018-03-05 11:59:06 +0100

    data processing: match season & order date

--- tests/base.py
+++ tests/base.py
@@ -10,14 +10,31 @@ import time
 # import os
 
 
-def funk_mask(d):
+def order_match_season(order):
+    seasons = dict()
+    seasons["Automne/Hiver"] = ["10", "11", "12", "01", "02", "03"]
+    seasons["Printemps/Eté"] = ["04", "05", "06", "07", "08", "09"]
+
+    if order.OrderCreationDate.split("-")[1] in seasons[order.SeasonLabel]:
+        return 1.0
+    return 0.0
+
+
+def funk_mask(orders, customers, products):
     """ Defining a simple mask over the input data """
     columns_ext = ["OrderCreationDate", "OrderNumber", "VariantId",
                    "CustomerId", "OrderCreationDate", "OrderShipDate",
                    "BillingPostalCode"]
 
+    # m = pd.merge(orders,
+    #              products.loc[:, ["VariantId", "SeasonLabel"]],
+    #              on='VariantId')
+
+    m = orders
+    m["MatchSeason"] = [order_match_season(o) for i, o in orders.iterrows()]
+
     # Remove columns from input array
-    x1 = d.loc[:, [xx for xx in d.columns if xx not in columns_ext]]
+    x1 = m.loc[:, [xx for xx in m.columns if xx not in columns_ext]]
 
     # Convert UnitPMPEur column to floats (price of a unit, in euros)
     x1.UnitPMPEUR = [np.float(x.replace(",", ".")) for x in x1.UnitPMPEUR]
@@ -48,8 +65,8 @@ def main():
     log("enter main")
 
     t = time.time()
-    # customers = pd.read_csv("../data/customers.csv")
-    # products = pd.read_csv("../data/products.csv")
+    customers = pd.read_csv("../data/customers.csv")
+    products = pd.read_csv("../data/products.csv")
     x_train = pd.read_csv("../data/X_train.csv")
     y_train = pd.read_csv("../data/y_train.csv")
     x_test = pd.read_csv("../data/X_test.csv")
@@ -57,8 +74,8 @@ def main():
     log("files loaded", t)
 
     t = time.time()
-    x1 = funk_mask(x_train)
-    x2 = funk_mask(x_test)
+    x1 = funk_mask(x_train, customers, products)
+    x2 = funk_mask(x_test, customers, products)
     log("applied mask", t)
 
     t = time.time()
@@ -67,17 +84,19 @@ def main():
     log("fit classifier", t)
 
     yres = clf.predict_proba(x1.loc[:100000, x1.columns])
-    print("train score:", roc_auc_score(y_train.ReturnQuantityBin.iloc[:100001],
-                                        yres[:, 1]))
-
+    score_train = roc_auc_score(
+        y_train.ReturnQuantityBin.iloc[:100001],
+        yres[:, 1])
     y_tosubmit = clf.predict_proba(x2.loc[:100000, x1.columns])
-    print("test score:", roc_auc_score(y_test.ReturnQuantityBin.iloc[:100001],
-                                       y_tosubmit[:, 1]))
+    score_test = roc_auc_score(
+        y_test.ReturnQuantityBin.iloc[:100001],
+        y_tosubmit[:, 1])
+
+    print("train score:", score_train)
+    print("test score: ", score_test)
 
 
 def check_value_error(m):
-    columns = [c for c in m.columns if m[c].dtype == 'float64']
-    print(m.loc[:, columns].dtypes)
     print("Any NaN?\t", np.any(np.isnan(m)))
     print("All finite?\t", np.all(np.isfinite(m)))
 
